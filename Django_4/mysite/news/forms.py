fbfacaae8a896bbd751eea913c70fb000462490d
Timon55 <forjob55@bk.ru>
2022-03-12 13:35:37 +0700

    Шаг 47. Добавил авторизацию и деавторизацию. Теперь в случае успешной регистрации пользователь автоматически авторизуется и перенаправляется на главную страницу.

--- Django_4/mysite/news/forms.py
+++ Django_4/mysite/news/forms.py
@@ -1,10 +1,14 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 from .models import News
 import re
 from django.forms import ValidationError
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Пароль', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(label='Имя пользователя', help_text='Максимум 150 символов', widget=forms.TextInput(attrs={'class': 'form-control'}))
--- Django_4/mysite/news/urls.py
+++ Django_4/mysite/news/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('', NewsList.as_view(), name='home'),
